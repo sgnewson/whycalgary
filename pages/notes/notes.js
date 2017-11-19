72478f0613a086de6e6a3b26f38dccbaa500cecd
Scott Newson <scott.g.newson@gmail.com>
2017-11-18 20:19:45 -0700

    add ability to take a photo on the notes page

--- pages/notes/notes.js
+++ pages/notes/notes.js
@@ -1,9 +1,38 @@
 Page({
 
   data: {
+    src: []
+    //TODO: save note text
   },
 
   onLoad: function () {
+  },
+
+  takephoto: function () {
+    var that = this
+    // Upload an image
+    wx.chooseImage({
+      count: 1,
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'],
+      success: function (res) {
+        console.log('success')
+        that.setData({
+          src: res.tempFilePaths
+        })
+        // Get image info
+        wx.getImageInfo({
+          //TODO: actually save the image
+          src: res.tempFilePaths[0],
+          success: function (res) {
+            console.log(res.width)
+            console.log(res.height)
+            console.log(res.path)
+          }
+        })
+      }
+    })
+
   }
 
 })
