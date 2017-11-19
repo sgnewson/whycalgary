71e9390d7414a878dd64691e3936565c73b08c60
Leca <lebaokhanh1991@gmail.com>
2017-11-18 17:55:38 -0700

    show real map (temp)

--- pages/map/map.js
+++ pages/map/map.js
@@ -3,6 +3,14 @@ const app = getApp()
 Page({
   data: {
     motto: 'Show map',
+    markers: [{
+      iconPath: "../../image/hospital_icon.png",
+      id: 0,
+      latitude: 40.002607,
+      longitude: 116.487847,
+      width: 35,
+      height: 45
+    }],
   },
   maptap: function() {
     console.log("Aloha");
