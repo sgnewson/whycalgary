cd35ea5866372e84a6b47e3a439c8516458d8bb2
Scott Newson <scott.g.newson@gmail.com>
2017-11-18 18:55:06 -0700

    allow phone calls and navigation from map detail page

--- pages/map-detail/map-detail.js
+++ pages/map-detail/map-detail.js
@@ -2,16 +2,26 @@ Page({
 
   data: {
     phoneNumber: '+1 (403) 955-7211',
-    address: '2888 Shaganappi Trail NW'
+    address: '2888 Shaganappi Trail NW',
+    longitude: 51.07455988,
+    latitude: -114.1479576,
+    scale: 15
   },
 
   onLoad: function (options) {
   },
 
   phonetap: function () {
-    console.log("phoned");
     wx.makePhoneCall({
       phoneNumber: this.data.phoneNumber,
+    })
+  },
+
+  addresstap: function () {
+    wx.openLocation({
+      latitude: this.data.latitude,
+      longitude: this.data.longitude,
+      scale: this.data.scale
     })
   }
 })
