b983e990e8216afb1ba572c812e5048369174637
Scott Newson <scott.g.newson@gmail.com>
2017-11-19 09:26:38 -0700

    Add preview of photo taken

--- pages/notes/notes.js
+++ pages/notes/notes.js
@@ -1,8 +1,7 @@
 Page({
 
   data: {
-    src: []
-    //TODO: save note text
+    src: ''
   },
 
   onLoad: function () {
@@ -10,29 +9,15 @@ Page({
 
   takephoto: function () {
     var that = this
-    // Upload an image
     wx.chooseImage({
       count: 1,
       sizeType: ['original', 'compressed'],
       sourceType: ['album', 'camera'],
       success: function (res) {
-        console.log('success')
         that.setData({
-          src: res.tempFilePaths
-        })
-        // Get image info
-        wx.getImageInfo({
-          //TODO: actually save the image
-          src: res.tempFilePaths[0],
-          success: function (res) {
-            console.log(res.width)
-            console.log(res.height)
-            console.log(res.path)
-          }
+          src: res.tempFilePaths[0]
         })
       }
     })
-
   }
-
 })
