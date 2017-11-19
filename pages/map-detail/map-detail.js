3f3ada0ad3fec9f7b50d9271241c033398958128
Leca <lebaokhanh1991@gmail.com>
2017-11-18 19:58:57 -0700

    bind data to map_desc

--- pages/map-detail/map-detail.js
+++ pages/map-detail/map-detail.js
@@ -1,9 +1,26 @@
 Page({
 
   data: {
+    name: "default",
+    description: "default",
+    address: "default",
+    tel_number: "default",
+    hours: "default",
+    specialty: "default",
+    waiting_time: "default"
   },
 
   onLoad: function (options) {
+    // console.log(options)
+    this.setData({
+      name: options.name,
+      description: options.description,
+      address: options.address,
+      tel_number: options.tel_number,
+      hours: options.hours,
+      specialty: options.specialty,
+      waiting_time: options.waiting_time
+    })
   },
 
   onReady: function () {
--- pages/map/map.js
+++ pages/map/map.js
@@ -3,13 +3,24 @@ const app = getApp()
 Page({
   data: {
     motto: 'Show map',
+    longitude: '-114.3543437',
+    latitude: '51.013214',
     markers: [{
       iconPath: "../../image/hospital_icon.png",
       id: 0,
-      latitude: 40.002607,
-      longitude: 116.487847,
+      latitude: 51.07455988,
+      longitude: -114.1479576,
       width: 35,
-      height: 45
+      height: 45,
+      location_detail: {
+        name: "Alberta Children's Hospital",
+        description: "hospital_desc",
+        address: "2888 Shaganappi Trail NW",
+        tel_number: "123456789",
+        hours: "9AM - 5PM",
+        specialty: "teeth",
+        waiting_time: "15 mins"
+      }
     }],
   },
   maptap: function() {
@@ -17,6 +28,22 @@ Page({
     wx.navigateTo({
       url: '../map-detail/map-detail',
     })
+  },
+  markertap: function(e) {
+    console.log(this.data.markers[e.markerId].location_detail)
+    var l_detail = this.data.markers[e.markerId].location_detail
+    wx.navigateTo({
+      url: '../map-detail/map-detail?name=' + l_detail.name
+        + '&description=' + l_detail.description
+        + '&address=' + l_detail.address
+        + '&tel_number=' + l_detail.tel_number
+        + '&hours=' + l_detail.hours
+        + '&specialty=' + l_detail.specialty
+        + '&waiting_time=' + l_detail.waiting_time
+    })
+    // wx.showToast({
+    //   // title: this.data.markers[e.markerId].location_detail
+    // });
   },
   onLoad: function () {
   }
