241c6aeec8e073d71155838f60a0d1a0739de89c
Leca <lebaokhanh1991@gmail.com>
2017-11-18 17:34:03 -0700

    map shows navigation

--- pages/index/index.js
+++ pages/index/index.js
@@ -13,6 +13,17 @@ Page({
     //   url: '../logs/logs'
     // })
   },
+  textTap: function() {
+    console.log("textTap")
+    wx.openLocation({
+      latitude: 51.0745598803388, // Latitude, floating number, between 90 and -90
+      longitude: -114.147957615112, // Longitude, floating number, between 180 and -180
+      name: "Alberta Children's Hospital", // Location name
+      address: '2888 Shaganappi Trail NW', // Detailed description of address
+      scale: 15, // Map scale level, integer value, between 1 and 28. It is the maximum value by default.
+      infoUrl: 'childrenshospital.ab.ca' // Hyperlink displayed at the bottom of the location viewing interface, which can be clicked to view the URL
+    });
+  },
   onLoad: function () {
     if (app.globalData.userInfo) {
       this.setData({
